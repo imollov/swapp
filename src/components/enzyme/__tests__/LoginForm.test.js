846acf461c8bb4f875d570ece6481131c05cee15
imollov <ivomollov@gmail.com>
2019-10-06 12:21:00 +0300

    LoginForm unit test

---
+++ src/components/enzyme/__tests__/LoginForm.test.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import LoginForm from '../../LoginForm'
+
+describe('LoginForm Component', () => {
+  it('smoke test', () => {
+    const wrapper = shallow(<LoginForm />)
+
+    expect(wrapper).toExist()
+  })
+
+  it('should match snapshot', () => {
+    const wrapper = shallow(
+      <LoginForm
+        username="demo"
+        password="qwerty"
+        onInputChange={() => {}}
+        onSubmit={() => {}}
+      />,
+    )
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should propagate input change', () => {
+    const onInputChangeMock = jest.fn()
+    const wrapper = shallow(
+      <LoginForm
+        username="demo"
+        password="qwerty"
+        onInputChange={onInputChangeMock}
+        onSubmit={() => {}}
+      />,
+    )
+
+    expect(onInputChangeMock).not.toHaveBeenCalled()
+
+    const usernameInput = wrapper.find('input[name="username"]')
+    usernameInput.simulate('change', {
+      target: { name: 'username', value: 'test' },
+    })
+
+    expect(onInputChangeMock).toHaveBeenCalledWith({
+      username: 'test',
+    })
+  })
+
+  it('should propagate submit', () => {
+    const onSubmitMock = jest.fn()
+    const wrapper = shallow(
+      <LoginForm
+        username="demo"
+        password="qwerty"
+        onInputChange={() => {}}
+        onSubmit={onSubmitMock}
+      />,
+    )
+
+    expect(onSubmitMock).not.toHaveBeenCalled()
+
+    const submitButton = wrapper.findWhere(
+      el => el.text() === 'Login' && el.type() === 'button',
+    )
+    submitButton.simulate('click')
+
+    expect(onSubmitMock).toHaveBeenCalled()
+  })
+})
