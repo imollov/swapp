2951ae44bd3147bc7da71c4992f06c360509f05a
imollov <ivomollov@gmail.com>
2019-11-02 17:49:34 +0200

    Button stories with theme knob

--- src/stories/1-Button.stories.js
+++ src/stories/1-Button.stories.js
@@ -1,22 +1,32 @@
 import React from 'react'
+import { withKnobs, select } from '@storybook/addon-knobs'
+
 import { Button } from 'rebass/styled-components'
 import { ThemeProvider } from 'styled-components'
+
 import GlobalStyle, { theme } from '../globalStyles'
 
 export default {
   title: 'Button',
 }
 
-export const primary = () => (
-  <ThemeProvider theme={{ ...theme }}>
-    <Button variant="primary">Boop</Button>
-    <GlobalStyle />
-  </ThemeProvider>
-)
-
-export const secondary = () => (
-  <ThemeProvider theme={{ ...theme }}>
-    <Button variant="secondary">Boop</Button>
-    <GlobalStyle />
-  </ThemeProvider>
-)
+export const Primary = () => {
+  withKnobs()
+  const mode = select('Theme', { dark: 'dark', light: 'light' }, 'dark')
+  return (
+    <ThemeProvider theme={{ ...theme(mode) }}>
+      <Button variant="primary">Boop</Button>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
+export const Secondary = () => {
+  withKnobs()
+  const mode = select('Theme', { dark: 'dark', light: 'light' }, 'dark')
+  return (
+    <ThemeProvider theme={{ ...theme(mode) }}>
+      <Button variant="secondary">Boop</Button>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
