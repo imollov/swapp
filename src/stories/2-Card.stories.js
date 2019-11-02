18d1cac2fc412959b03771d8c07e889c205fcb1b
imollov <ivomollov@gmail.com>
2019-11-02 20:36:23 +0200

    Episode card story

---
+++ src/stories/2-Card.stories.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { withKnobs, select } from '@storybook/addon-knobs'
+
+import EpisodeCard from '../pages/episodes/components/EpisodeCard'
+
+import { ThemeProvider } from 'styled-components'
+import GlobalStyle, { theme } from '../globalStyles'
+
+export default {
+  title: 'Card',
+}
+
+const title = 'The Phantom Menance'
+const desc =
+  'Turmoli has engulfed the Galactic Republic. ' +
+  'The taxation of trade routes to outlying star systems is in dispute. ' +
+  'Hoping to resolve the matter with a blockade of deadly battleships, the greedy...'
+const img =
+  'https://m.media-amazon.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_UX182_CR0,0,182,268_AL_.jpg'
+
+export const Movie = () => {
+  withKnobs()
+  const mode = select('Theme', { dark: 'dark', light: 'light' }, 'dark')
+  return (
+    <ThemeProvider theme={{ ...theme(mode) }}>
+      <EpisodeCard title={title} text={desc} img={img} width={256} />
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
