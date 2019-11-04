805a485bffe1e0cf62af917d586da1d7c53c533f
imollov <ivomollov@gmail.com>
2019-11-05 00:40:51 +0200

    Use theme provider in App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react'
-import { ThemeProvider } from 'styled-components'
-
-import GlobalStyle, { theme } from './globalStyles'
+import ThemeProvider from './components/ThemeProvider'
 
 import AppBar from './components/AppBar'
 import EpisodePage from './pages/episodes/pages/episode'
 
 const App = () => {
   return (
-    <ThemeProvider theme={{ ...theme('dark') }}>
+    <ThemeProvider>
       <AppBar />
       <EpisodePage />
-      <GlobalStyle />
     </ThemeProvider>
   )
 }
