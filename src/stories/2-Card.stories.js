34f6b0a00ec4ccba0983355b32188aaf9214f9de
imollov <ivomollov@gmail.com>
2019-11-05 13:12:35 +0200

    Use json data export for stories

--- src/stories/2-Card.stories.js
+++ src/stories/2-Card.stories.js
@@ -10,73 +10,87 @@ import InfoCard from '../components/InfoCard'
 import ThemeProvider from '../components/ThemeProvider'
 import ThemedBox from './common/ThemedBox'
 
+import episodesData from './data/episodes.json'
+import personsData from './data/persons'
+import starshipsData from './data/starships'
+
 export default {
   title: 'Card',
 }
 
-const title = 'The Phantom Menance'
-const subtitle = 'Star Wars: Episode I'
-const desc =
-  'Turmoli has engulfed the Galactic Republic. ' +
-  'The taxation of trade routes to outlying star systems is in dispute. ' +
-  'Hoping to resolve the matter with a blockade of deadly battleships, the greedy...'
-const details = [['Director', 'George Lucas'], ['Release Date', '1999-05-19']]
-const img =
-  'https://m.media-amazon.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_UX182_CR0,0,182,268_AL_.jpg'
+const {
+  data: {
+    allEpisodes: {
+      edges: {
+        3: { node: episode1 },
+      },
+    },
+  },
+} = episodesData
 
 export const Preview = () => {
   return (
     <ThemeProvider>
       <ThemedBox width={256}>
-        <EpisodePreview title={title} text={desc} img={img} />
+        <EpisodePreview
+          title={episode1.title}
+          text={episode1.openingCrawl}
+          img={episode1.image}
+        />
       </ThemedBox>
     </ThemeProvider>
   )
 }
 
 export const Banner = () => {
+  const title = `Star Wars: Episode ${episode1.episodeId}`
   return (
     <ThemeProvider>
       <ThemedBox width={940}>
-        <EpisodeBanner title={title} subtitle={subtitle} img={img} />
+        <EpisodeBanner
+          title={episode1.title}
+          subtitle={title}
+          img={episode1.image}
+        />
       </ThemedBox>
     </ThemeProvider>
   )
 }
 
 export const Info = () => {
+  const details = [
+    ['Director', episode1.director],
+    ['Release Date', episode1.releaseDate],
+  ]
   return (
     <ThemeProvider>
       <ThemedBox width={940}>
-        <EpisodeInfo info={desc + desc + desc} details={details} />
+        <EpisodeInfo info={episode1.openingCrawl} details={details} />
       </ThemedBox>
     </ThemeProvider>
   )
 }
 
-const characterName = 'Han Solo'
-const characterImg =
-  'https://i.pinimg.com/originals/19/37/95/19379598fbb4338dd02e7ea8dde3ad63.jpg'
-const characterDetails = [
-  ['Weight', '80'],
-  ['Height', '180'],
-  ['Home world', 'Corellia'],
-  ['Species', 'Human'],
-]
-
-const starshipName = 'Millennium Falcon'
-const starshipImg =
-  'https://cdn3.volusion.com/bmfcy.fjqhr/v/vspfiles/photos/BANSW219770-2.jpg?1533738074'
+const {
+  data: {
+    allPeople: { edges: persons },
+  },
+} = personsData
 
 export const InfoWithImage = () => {
+  const {
+    5: { node: person },
+  } = persons
+  const personDetails = [
+    ['Height', person.height],
+    ['Weight', person.mass],
+    ['Home world', person.homeworld.name],
+    ['Species', person.species.name],
+  ]
   return (
     <ThemeProvider>
       <ThemedBox width={470}>
-        <InfoCard
-          title={characterName}
-          img={characterImg}
-          data={characterDetails}
-        />
+        <InfoCard title={person.name} img={person.image} data={personDetails} />
       </ThemedBox>
     </ThemeProvider>
   )
@@ -87,40 +101,35 @@ export const List3Columns = () => {
     <ThemeProvider>
       <ThemedBox width={940}>
         <ResponsiveList hasMore={true}>
-          <ImageCard img={characterImg} title={characterName} height={100} />
-          <ImageCard img={characterImg} title={characterName} height={100} />
-          <ImageCard img={characterImg} title={characterName} height={100} />
-          <ImageCard img={characterImg} title={characterName} height={100} />
-          <ImageCard img={characterImg} title={characterName} height={100} />
+          {persons.map(({ node: p }) => (
+            <ImageCard img={p.image} title={p.name} height={100} />
+          ))}
         </ResponsiveList>
       </ThemedBox>
     </ThemeProvider>
   )
 }
 
+const {
+  data: {
+    allStarships: { edges },
+  },
+} = starshipsData
+const starships = edges.slice(0, 4)
+
 export const List1Column = () => {
   return (
     <ThemeProvider>
       <ThemedBox width={470}>
         <ResponsiveList maxColumns={1}>
-          <ImageCard
-            variant="compact"
-            img={starshipImg}
-            title={starshipName}
-            height={60}
-          />
-          <ImageCard
-            variant="compact"
-            img={starshipImg}
-            title={starshipName}
-            height={60}
-          />
-          <ImageCard
-            variant="compact"
-            img={starshipImg}
-            title={starshipName}
-            height={60}
-          />
+          {starships.map(({ node: s }) => (
+            <ImageCard
+              variant="compact"
+              img={s.image}
+              title={s.name}
+              height={60}
+            />
+          ))}
         </ResponsiveList>
       </ThemedBox>
     </ThemeProvider>
