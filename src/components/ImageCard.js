9d2b278b01d74d626fc3b3fb243e645b4260ea7e
imollov <ivomollov@gmail.com>
2019-11-05 16:43:43 +0200

    Background image component

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Flex, Heading, Box, Image } from 'rebass/styled-components'
+import { Flex, Heading, Box } from 'rebass/styled-components'
+import BackgroundImage from './BackgroundImage'
 
 export default ({ variant, img, title, ...rest }) => {
   const imageWidth = variant === 'compact' ? 1 / 5 : 1 / 3
   return (
     <Flex {...rest} variant="card">
-      <Image src={img} width={imageWidth} />
-      <Box margin="auto" p={1} textAlign="center">
+      <BackgroundImage src={img} width={imageWidth} />
+      <Box margin="auto" p={2} textAlign="center">
         <Heading variant="h3">{title}</Heading>
       </Box>
     </Flex>
