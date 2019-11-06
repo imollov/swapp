067869d6376ffb8defd4f5ff3e9f961da60d00ed
Ivelin Mollov <ivomollov@gmail.com>
2019-11-06 10:51:53 +0200

    Split dark and light color modes (#17)

---
+++ src/theme/dark.js
@@ -0,0 +1,20 @@
+import palette from './palette'
+
+export default {
+  primary: palette.yellow,
+  secondary: palette.lightBlue,
+  text: '#abb1ba',
+  background: palette.black,
+  muted: '#333',
+  buttonBg: palette.lightBlue,
+  borderColor: '#3C4858',
+  subHeading: palette.lightBlue,
+  detailLabel: palette.lightBlue,
+  detailValue: palette.yellow,
+  appBar: '#333',
+  inputBg: palette.white,
+  inputBorder: '#3C4858',
+  inputColor: '#273444',
+  logo: palette.yellow,
+  divider: '#999',
+}
