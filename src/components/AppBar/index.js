bb69c14f4fd5b4f89a10be2b4a9a7d9c308553ca
imollov <ivomollov@gmail.com>
2019-11-08 20:54:39 +0200

    Separate AppBar presentational component

--- src/components/AppBar/index.js
+++ src/components/AppBar/index.js
@@ -1,10 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { useApolloClient } from '@apollo/react-hooks'
-import { Flex, Box, Text } from 'rebass/styled-components'
-
-import Logo from '../Logo'
-import Logout from './Logout'
+import AppBar from './AppBar'
 
 export default props => {
   const client = useApolloClient()
@@ -14,24 +10,5 @@ export default props => {
     client.writeData({ data: { authenticated: false } })
   }
 
-  return (
-    <Flex {...props} bg="appBar" height={60}>
-      <Box width={1 / 2} pl={4} alignSelf="center">
-        <Logo variant="logo.small" />
-      </Box>
-      <Flex width={1 / 2} justifyContent="flex-end" alignSelf="center">
-        <Box mr={3} alignSelf="center">
-          <Link to="/episodes">
-            <Text variant="link">Episodes</Text>
-          </Link>
-        </Box>
-        <Box mr={3} alignSelf="center">
-          <Link to="/characters">
-            <Text variant="link">Characters</Text>
-          </Link>
-        </Box>
-        <Logout mr={3} onClick={logout} />
-      </Flex>
-    </Flex>
-  )
+  return <AppBar logout={logout} {...props} />
 }
