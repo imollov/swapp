df9c18d30ba2cbb9bca57df96e496dcc81063f81
imollov <ivomollov@gmail.com>
2019-11-08 20:53:46 +0200

    Add story router decorator

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,9 @@
 import { configure, addDecorator } from '@storybook/react'
 import { withKnobs } from '@storybook/addon-knobs'
+import StoryRouter from 'storybook-react-router'
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../src/stories', true, /\.stories\.js$/), module)
 
+addDecorator(StoryRouter())
 addDecorator(withKnobs)
